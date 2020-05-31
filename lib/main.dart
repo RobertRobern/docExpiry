db3a5c8ace4d89f0220b813b2b6e0ad4e6c86173
RobertRobern <30721349+RobertRobern@users.noreply.github.com>
2020-05-31 21:23:17 +0300

    Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:docExpiry/ui/doclist.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(DocExpiryApp());
@@ -7,44 +8,9 @@ class DocExpiryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'DocExpiry',
+      // title: 'DocExpiry',
       theme: new ThemeData(primarySwatch: Colors.cyan),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'DOC EXPIRY',
-            style: TextStyle(fontSize: 25, color: Colors.white),
-          ),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(8.0),
-          width: double.infinity,
-          height: 100,
-          child: Material(
-            elevation: 6.0,
-            borderRadius: BorderRadius.circular(10),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-                top: 5,
-                bottom: 5,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Test1',
-                    style: TextStyle(fontSize: 25.0),
-                  ),
-                  Text('0 days left', style: TextStyle(fontSize: 20.0)),
-                  Text('Exp 16 May 2020', style: TextStyle(fontSize: 15.0)),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
+      home: DocList(),
     );
   }
 }
